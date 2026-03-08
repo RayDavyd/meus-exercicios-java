89c7cf956480fa09721963a17452cc418bb5777a
RayDavyd <raydavyd8@gmail.com>
2026-03-07 21:35:52 -0300

    calculo de area do circulo com quatro casas decimais

--- exercicio_03/src/Main.java
+++ exercicio_03/src/Main.java
@@ -1,5 +1,27 @@
+//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+//casas decimais conforme exemplos.
+//Fórmula da área: area = π . raio2
+//Considere o valor de π = 3.14159
+
+
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args){
+    static final double VALOR_PI = 3.14159;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o valor do raio de um circulo: ");
+        double raio = sc.nextDouble();
+
+        double area = VALOR_PI * Math.pow(raio, 2);
+
+        System.out.printf("O valor da area do circulo é: %.4f\n", area);
+
+        sc.close();
+
 
     }
 }
